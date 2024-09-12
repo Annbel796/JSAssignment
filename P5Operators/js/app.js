6352b4ffa757aa3f128d60a2a15ed6f724f3cbc0
Annbel796 <annbel796@ihmeducation.se>
2024-09-12 15:31:35 +0200

    Matematics

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -1,14 +1,38 @@
 let speedInput = 72;
 let speedOutput = 0;
 speedOutput = speedInput / 3.6;
-console.log(speedOutput);
+console.log(speedOutput); // 20
 
 let minutesInpute = 3;
 let secondsOutpute = 0;
 secondsOutpute = minutesInpute * 60;
-console.log(secondsOutpute);
+console.log(secondsOutpute); // 180
 
-let numberInpute =
-  
+let divisionInput1 = 11;
+let divisionInput2 = 4;
+let divisionOutput = 0;
+divisionOuteput = divisionInput1 / divisionInput2;
+console.log(divisionOutput); // 2.75
+
+let numberInput = 11;
+let numberOutput = 0;
+numberOutpute = numberInpute %= 4;
+console.log(numberOutpute); // 3
+
+let radius = 2;
+let area = 0;
+area = 3.14*(radius*radius);
+console.log(area); // 12.56
+
+let nrInput = 11;
+let nrOutput =0;
+nrOutput = -(nrInput);
+console.log(nrOutput); //-11
+
+Implement a program that calculates the BMI of a person using their weight in kilograms and height in meters.
+  Display the BMI value.
+○ Input: 70, 1,82
+○ Output: 21,13…
+
 
 
