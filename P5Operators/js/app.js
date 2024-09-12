8791f761d3ff19573f50e33c41704f3f819b4157
Annbel796 <annbel796@ihmeducation.se>
2024-09-12 13:52:16 +0200

    Changes P5

---
+++ P5Operators/js/app.js
@@ -0,0 +1,24 @@
+let x = 5 + 5; // x = 10
+let y = x + 5; // y = 15
+let z = y + y; // z = 30
+console.log(z + z); // 60
+
+let firstNumber = 7;
+let secondNumber = 2;
+let answer = 0;
+
+// 1 + 2 = 3
+answer = firstNumber + secondNumber;
+console.log (answer);
+
+answer = firstNumber - secondNumber;
+console.log(answer);
+
+answer = firstNumber * secondNumber;
+console.log(answer);
+
+answer = firstNumber / secondNumber;
+console.log(answer);
+
+answer = firstNumber % secondNumber;
+console.log(answer);
