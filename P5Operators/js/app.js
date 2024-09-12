182f1186664e99f6ac6eba15673d0b9855e0678a
Annbel796 <annbel796@ihmeducation.se>
2024-09-12 14:42:00 +0200

    Shift app to app2

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -1,3 +1,4 @@
+//USING 2 VARIABLES
 let firstNumber = 7;
 let secondNumber = 2;
 let answer = 0;
@@ -39,6 +40,33 @@ console.log(gold); // 60
 
 gold %= 2;
 console.log(gold); // 0
+
+let numberOfStudents = 0;
+
+numberOfStudents += 1;
+console.log(numberOfStudents); // 1
+
+numberOfStudents ++;
+console.log(numberOfStudents); // 2
+
+++numberOfStudents;
+console.log(numberOfStudents); // 3
+
+console.log(numberOfStudents++); // 3 why
+console.log(numberOfStudents); // 4 Why
+
+console.log(++numberOfStudents); // 5
+
+numberOfStudents --;
+console.log(numberOfStudents); // 1
+
+// Implement a program that takes an input for km/h and displays the result in m/s.
+// ○ Input: 72
+// ○ Output: 20
+
+let speedKmh = 72;
+let speedMs = convertKmHToMs(speedKmh);
+console.log(speedMs);
 
 
 
