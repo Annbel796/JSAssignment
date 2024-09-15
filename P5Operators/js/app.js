328243c1876f32573e6fa30be7f31fb11be75eb0
Annbel796 <annbel796@ihmeducation.se>
2024-09-15 18:26:16 +0200

    Сompleted Task

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -10,14 +10,12 @@ console.log(secondsOutpute); // 180
 
 let divisionInput1 = 11;
 let divisionInput2 = 4;
-let divisionOutput = 0;
-divisionOuteput = divisionInput1 / divisionInput2;
-console.log(divisionOutput); // 2.75
-
-let numberInput = 11;
-let numberOutput = 0;
-numberOutpute = numberInpute %= 4;
-console.log(numberOutpute); // 3
+let divisionOutput1 = 0;
+let divisionOutput2 = 0;
+divisionOutput1 = divisionInput1 / divisionInput2;
+divisionOutput2 = divisionInput1 %= 4;
+console.log(divisionOutput1); // 2.75
+console.log(divisionOutput2); // 3
 
 let radius = 2;
 let area = 0;
@@ -25,14 +23,29 @@ area = 3.14*(radius*radius);
 console.log(area); // 12.56
 
 let nrInput = 11;
-let nrOutput =0;
+let nrOutput = 0;
 nrOutput = -(nrInput);
 console.log(nrOutput); //-11
 
-Implement a program that calculates the BMI of a person using their weight in kilograms and height in meters.
-  Display the BMI value.
-○ Input: 70, 1,82
-○ Output: 21,13…
+let weightInput = 1.82;
+let kiloInput = 70;
+BMIOutput = kiloInput/(weightInput*weightInput);
+console.log(BMIOutput); // 21,13
+
+let lengthInput1 = 3;
+let lengthInput2 = 4;
+let hypotenuseOutput = 0;
+hypotenuseOutput = Math.sqrt(lengthInput1*lengthInput1+lengthInput2*lengthInput2);
+console.log(hypotenuseOutput); // 5
+
+let secondsInput = 111;
+let minutesOutput = 0;
+let secondsOutput = 0;
+minutesOutput = Math.floor (secondsInput / 60);
+secondsOutput = secondsInput % 60;
+console.log(minutesOutput,"minute(s) and", secondsOutput,"second(s)");
+
+// 1 minute(s) and 51 second(s)
 
 
 
