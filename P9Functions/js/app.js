510f4f7ca9b10f2b62990f0f6c055fd38c213b41
Annbel796 <annbel796@ihmeducation.se>
2024-09-16 14:30:42 +0200

    New Project

---
+++ P9Functions/js/app.js
@@ -0,0 +1,11 @@
+function makeLasagna() {
+  console.log("Fry meat.");
+  console.log("Cut onions.");
+  console.log("Fry onions.");
+  console.log("Add tomato sauce.");
+  console.log("Add spices.");
+  console.log("Boil.");
+}
+
+makeLasagna();
+
