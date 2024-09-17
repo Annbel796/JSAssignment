061d836cdee4ea0b32e477e221bd812d95cf55da
Annbel796 <annbel796@ihmeducation.se>
2024-09-17 10:39:10 +0200

    Added Task Part 1

---
+++ P10OutputAndInput/js/app.js
@@ -0,0 +1,25 @@
+const number = 4;
+function doubleNumber(numberToDouble) {
+  return numberToDouble * 2;
+}
+console.log(doubleNumber(number)); // Output: 8
+
+let inputeNomberOne = 0;
+
+function multiplyBySelf (numberTwoMoltiply) {
+  return numberTwoMoltiply * numberTwoMoltiply;
+}
+console.log(multiplyBySelf(4))
+
+
+function average(firstNumber,secondNumber) {
+  return (firstNumber + secondNumber)/2;
+}
+console.log(average(2,6));
+
+
+function addName(firstName, secondName) {
+  return (firstName + secondName).toString();
+}
+console.log("Welcome", addName("Ben ", "Ting"));
+
