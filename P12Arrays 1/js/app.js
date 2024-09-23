02c504d07651dbb5fa698a254b99988cd02705da
Annbel796 <annbel796@ihmeducation.se>
2024-09-23 11:53:55 +0200

    Added Example

---
+++ P12Arrays 1/js/app.js
@@ -0,0 +1,11 @@
+let studentNames = ["Olivia", "Ethan", "Isabella", "Emma", "Anna", "Lukas"];
+console.log("We have " + studentNames.length + " students");
+
+studentNames [2] = "Michael";
+studentNames.push("Ben");
+
+ for (let i =0; i <  studentNames.length; i++) {
+console.log("Good morning " + studentNames[i] + "!");
+}
+
+
