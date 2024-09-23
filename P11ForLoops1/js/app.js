dfb8cb559ed53d2b6b8a3869d280d3ec664563ae
Annbel796 <annbel796@ihmeducation.se>
2024-09-23 09:54:14 +0200

    Added Task

--- P11ForLoops1/js/app.js
+++ P11ForLoops1/js/app.js
@@ -1,8 +1,18 @@
 // Print 13 different random numbers between 1 and 6.
 // ○ Output: 1 6 2 5 4 6 2 5 1 3 4 4 6
 
-function getRandomInt(min, max) {
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
-for (let i =0; i < ; i++)
 
+for (var i =0; i < 13; i++){
+console.log(getRandomInt(6));
+}
+
+//Start at 1, multiply the number of each iteration by 2 and stop after 1024. Print each number
+// to the console.
+// ○ Output: 1 2 4 8 16 32 64 128 256 512 1024
+
+for (let i = 1; i < 1024 ; i*2) {
+  console.log(i);
+}
