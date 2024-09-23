c5c126e9db2e3b51702412c09216f6347478d3f8
Annbel796 <annbel796@ihmeducation.se>
2024-09-23 11:57:16 +0200

    Changes in Example

--- P12Arrays 1/js/app.js
+++ P12Arrays 1/js/app.js
@@ -3,6 +3,7 @@ console.log("We have " + studentNames.length + " students");
 
 studentNames [2] = "Michael";
 studentNames.push("Ben");
+console.log("We have " + studentNames.length + " students");
 
  for (let i =0; i <  studentNames.length; i++) {
 console.log("Good morning " + studentNames[i] + "!");
